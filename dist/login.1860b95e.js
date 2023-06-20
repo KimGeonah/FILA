ee1e714296e27f56665a3c8ef198320bc158afbd
KimGeonah <gunnnna@naver.com>
2023-06-20 10:35:39 +0900

    ver06

--- dist/login.1860b95e.js
+++ dist/login.1860b95e.js
@@ -137,120 +137,120 @@ var items = [{
   gender: "남자",
   name: "컴포트핏 인터내셔널 반팔 카라티",
   price: "69,000\uC6D0",
-  src: "./sub_img/item02.jpg",
-  srchover: "./sub_img/item02_hover.jpg",
+  src: "./img/sub_img/item02.jpg",
+  srchover: "./img/sub_img/item02_hover.jpg",
   color: "beige"
 }, {
   id: "item03",
   gender: "남자",
   name: "인터내셔널 반팔 카라티",
   price: "69,000\uC6D0",
-  src: "./sub_img/item03.jpg",
-  srchover: "./sub_img/item03_hover.jpg",
+  src: "./img/sub_img/item03.jpg",
+  srchover: "./img/sub_img/item03_hover.jpg",
   color: "blue"
 }, {
   id: "item04",
   gender: "공용",
   name: "스포츠 인터내셔널 로고 반팔티",
   price: "49,000\uC6D0",
-  src: "./sub_img/item04.jpg",
-  srchover: "./sub_img/item04_hover.jpg",
+  src: "./img/sub_img/item04.jpg",
+  srchover: "./img/sub_img/item04_hover.jpg",
   color: "white"
 }, {
   id: "item05",
   gender: "공용",
   name: "인터내셔널 그래픽 반팔티",
   price: "49,000\uC6D0",
-  src: "./sub_img/item05.jpg",
-  srchover: "./sub_img/item05_hover.jpg",
+  src: "./img/sub_img/item05.jpg",
+  srchover: "./img/sub_img/item05_hover.jpg",
   color: "black"
 }, {
   id: "item06",
   gender: "공용",
   name: "레터링 그래픽 티셔츠",
   price: "49,000\uC6D0",
-  src: "./sub_img/item06.jpg",
-  srchover: "./sub_img/item06_hover.jpg",
+  src: "./img/sub_img/item06.jpg",
+  srchover: "./img/sub_img/item06_hover.jpg",
   color: "white"
 }, {
   id: "item07",
   gender: "남자",
   name: "핫써머 Joyful 나시티",
   price: "45,000\uC6D0",
-  src: "./sub_img/item07.jpg",
-  srchover: "./sub_img/item07_hover.jpg",
+  src: "./img/sub_img/item07.jpg",
+  srchover: "./img/sub_img/item07_hover.jpg",
   color: "black"
 }, {
   id: "item08",
   gender: "남자",
   name: "테니스 퍼포먼스 반팔티셔츠",
   price: "49,000\uC6D0",
-  src: "./sub_img/item08.jpg",
-  srchover: "./sub_img/item08_hover.jpg",
+  src: "./img/sub_img/item08.jpg",
+  srchover: "./img/sub_img/item08_hover.jpg",
   color: "black"
 }, {
   id: "item09",
   gender: "남자",
   name: "테니스 퍼포먼스 카라티셔츠",
   price: "79,000\uC6D0",
-  src: "./sub_img/item09.jpg",
-  srchover: "./sub_img/item09_hover.jpg",
+  src: "./img/sub_img/item09.jpg",
+  srchover: "./img/sub_img/item09_hover.jpg",
   color: "black"
 }, {
   id: "item10",
   gender: "공용",
   name: "테니스 라이프 스테디움 자켓",
   price: "149,000\uC6D0",
-  src: "./sub_img/item10.jpg",
-  srchover: "./sub_img/item10_hover.jpg",
+  src: "./img/sub_img/item10.jpg",
+  srchover: "./img/sub_img/item10_hover.jpg",
   color: "green"
 }, {
   id: "item11",
   gender: "공용",
   name: "테잎 오버핏 바람막이 자켓",
   price: "159,000\uC6D0",
-  src: "./sub_img/item11.jpg",
-  srchover: "./sub_img/item11_hover.jpg",
+  src: "./img/sub_img/item11.jpg",
+  srchover: "./img/sub_img/item11_hover.jpg",
   color: "grey"
 }, {
   id: "item12",
   gender: "공용",
   name: "테니스 라이프 카라 맨투맨",
   price: "99,000\uC6D0",
-  src: "./sub_img/item12.jpg",
-  srchover: "./sub_img/item12_hover.jpg",
+  src: "./img/sub_img/item12.jpg",
+  srchover: "./img/sub_img/item12_hover.jpg",
   color: "green"
 }, {
   id: "item13",
   gender: "공용",
   name: "베이직 리니어 후드집업",
   price: "79,000\uC6D0",
-  src: "./sub_img/item13.jpg",
-  srchover: "./sub_img/item13_hover.jpg",
+  src: "./img/sub_img/item13.jpg",
+  srchover: "./img/sub_img/item13_hover.jpg",
   color: "grey"
 }, {
   id: "item14",
   gender: "공용",
   name: "헤리티지 레귤러핏 케이블 스웨터",
   price: "109,000\uC6D0",
-  src: "./sub_img/item14.jpg",
-  srchover: "./sub_img/item14_hover.jpg",
+  src: "./img/sub_img/item14.jpg",
+  srchover: "./img/sub_img/item14_hover.jpg",
   color: "white"
 }, {
   id: "item15",
   gender: "공용",
   name: "테잎 오버핏 우븐 반바지",
   price: "79,000\uC6D0",
-  src: "./sub_img/item15.jpg",
-  srchover: "./sub_img/item15_hover.jpg",
+  src: "./img/sub_img/item15.jpg",
+  srchover: "./img/sub_img/item15_hover.jpg",
   color: "green"
 }, {
   id: "item16",
   gender: "공용",
   name: "아티스트 이진 반바지",
   price: "59,000\uC6D0",
-  src: "./sub_img/item16.jpg",
-  srchover: "./sub_img/item16_hover.jpg",
+  src: "./img/sub_img/item16.jpg",
+  srchover: "./img/sub_img/item16_hover.jpg",
   color: "beige"
 }];
 
@@ -450,7 +450,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "52078" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "52670" + '/');
   ws.onmessage = function (event) {
     checkedAssets = {};
     assetsToAccept = [];
